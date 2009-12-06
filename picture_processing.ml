c24a533aa889d9fb65bd707ec1e44d400a30d91c
vergez.y <vergez.y@bb40ffec-b4f5-11de-b19d-3371f2377dbd>
2009-12-06 16:14:39 +0000

    One pix for edges

--- picture_processing.ml
+++ picture_processing.ml
@@ -11,10 +11,20 @@
 *******************************************************
 *)
 
+(*Compare les pixels adjacents*)
+
+let closed_pix src i j h w =
+  if ((i+1)<h) && ((j+1)<w) then
+   (Sdlvideo.get_pixel src (i)(j))=(Sdlvideo.get_pixel src (i+1)(j))
+   && (Sdlvideo.get_pixel src (i)(j))=(Sdlvideo.get_pixel src (i)(j-1))
+  else
+    false
+
+
 (*Cree la matrice de convolution 3/3 correspondante
 aux 8 pixels entourants le pixel courant.
 Pixel en dehors de l'image : valeur du pixel central.*)
-
+(*
 let create_matrix src i j h w =
   let mat = Array.make 9 0 in
   let cpt = ref 0 in
@@ -28,9 +38,9 @@ let create_matrix src i j h w =
       done;
     done;
     mat
-
+*)
 (*Applique le filtre de Sobel au pixel courant.*)
-
+(*
 let filter src i j h w =
   let mat = create_matrix src i j h w in
   let filterx = (-1)*mat.(0) + 1*mat.(2) + (-2)*mat.(3)
@@ -40,7 +50,7 @@ let filter src i j h w =
     + (-1)*mat.(6) + (-2)*mat.(7) + (-1)*mat.(8) in
   let res = (abs(filterx) + abs(filtery)) / 8 in
     res
-
+*)
 (*Supprime l'extension du fichier et son emplacement*)
 
 let del_ext s =
@@ -55,7 +65,10 @@ let del_ext s =
 (*Met la liste d'altitudes en ordre croissant*)
 
 let incr_list list =
-  (*let rec put_in_place = function
+  let aux (x,y,z,a) (u,v,w,b) = a-b in
+    List.sort aux list
+(*
+  let rec put_in_place = function
       [] -> []
     |e::[] -> e::[]
     |(r1,g1,b1,h1)::(r2,g2,b2,h2)::l ->
@@ -64,10 +77,17 @@ let incr_list list =
        else
 	 (r1,g1,b1,h1)::(put_in_place ((r2,g2,b2,h2)::l))
   in
-    for i = 0 to ((List.length list)/2) do
-      list = put_in_place list;
+  let r_list = ref list in
+    for i = 0 to ((List.length list) - 1) do
+      r_list := put_in_place !r_list;
     done;
-    list*)[]
+<<<<<<< .mine
+    !r_list
+=======
+    list[]
+>>>>>>> .r30*)
+
+
 
 (*Ecrit les differentes couleurs et leurs altitudes correspondantes
 dans un fichier texte "colors"*)
@@ -106,15 +126,15 @@ let process_img file out_file =
     Sdlvideo.lock dst;
     for j = 0 to (w-1) do
       for i = 0 to (h-1) do
-        if ((filter src i j h w) = 0) then
+        if (closed_pix src i j h w) then
           begin
 	    let color = Sdlvideo.get_pixel src i j in
               Sdlvideo.put_pixel dst i j color;
 	      let (r,g,b) = Sdlvideo.get_pixel_color src i j in
-		list := rf_cur_list (r,g,b) !list
+		list := rf_cur_list (r,g,b) !list;
           end
-        else
-          Sdlvideo.put_pixel_color dst i j Sdlvideo.black
+	else
+	  Sdlvideo.put_pixel_color dst i j Sdlvideo.black
       done;
     done;
 
