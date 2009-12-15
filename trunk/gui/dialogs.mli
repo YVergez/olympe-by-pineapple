c8a616445476029367b5c8ab37fb731a8407d89d
guillaume.algis <guillaume.algis@bb40ffec-b4f5-11de-b19d-3371f2377dbd>
2009-12-15 01:36:15 +0000

    Version 1.0

--- trunk/gui/dialogs.mli
+++ trunk/gui/dialogs.mli
@@ -1,9 +1,6 @@
 val showErrMessage : ?title:string -> text:'a -> unit -> unit
-val licence2string : string -> string
 val showAbout : unit -> unit
 val verifyFile : ?callback:(unit -> unit) -> string list -> unit
-val img_filter : GFile.filter
-val obj_filter : GFile.filter
 val showOpenFile : unit -> unit
 type file = IMAGE | OBJ
 val saveFile : string -> file -> unit
