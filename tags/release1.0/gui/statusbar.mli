ee250f25a5b421ac1532251c88e001ae3db0f595
guillaume.algis <guillaume.algis@bb40ffec-b4f5-11de-b19d-3371f2377dbd>
2009-12-15 02:35:02 +0000

    Still out of date, found another bug in the Makefile

--- tags/release1.0/gui/statusbar.mli
+++
@@ -1,5 +0,0 @@
-val statusbar_context : GMisc.statusbar_context ref
-val create : unit -> unit
-val clean : unit -> unit
-val setInfo : ?timeout:bool -> string -> unit
-val getContext : unit -> GMisc.statusbar_context ref
